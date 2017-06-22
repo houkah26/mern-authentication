db9c387af63adfb5e3517d28f5244ff6630359ee
houkah26 <houkah26@gmail.com>
2017-06-22 10:02:35 -0700

    remove console.log and edit author

--- server/config/passport.js
+++ server/config/passport.js
@@ -31,9 +31,7 @@ const jwtOptions = {
 };
 
 // Setting up JWT login strategy
-const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
-  console.log("payload:");
-  console.log(payload);  
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => { 
   User.findById(payload._id, (err, user) => {
     if (err) { return done(err, false); }
 
