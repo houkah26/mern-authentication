0d4e240415ec847661ec50c53c51422aed5af36e
houkah26 <houkah26@gmail.com>
2017-06-23 15:05:26 -0700

    add logout button

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';  
 
-const Header = () => (
+import { logoutUser } from '../../actions';
+
+const Header = ({ logoutUser, authenticated }) => (
   <div>
     <Link to="/">HomePage!</Link>
     <Link to="/register">Register</Link>
     <Link to="/login">Login</Link>
     <Link to="/dashboard">Dashboard</Link>
+    <button onClick={logoutUser}>Logout</button>
   </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => {
+  return { authenticated: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps, { logoutUser })(Header);
