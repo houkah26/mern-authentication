b4fa75077be3c09b2947f9b5409ede7a04a1a9c9
houkah26 <houkah26@gmail.com>
2017-06-23 15:04:59 -0700

    use push for changing the url

--- client/src/components/auth/RequireAuth.js
+++ client/src/components/auth/RequireAuth.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';  
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
 export default function(ComposedComponent) {  
   class Authentication extends Component {
     componentWillMount() {
       if(!this.props.authenticated) {
-        this.props.history.push("/login");
+       this.props.dispatch(push("/login"));
       }
     }
 
     componentWillUpdate(nextProps) {
       if(!nextProps.authenticated) {
-        this.props.history.push("/login");
+       this.props.dispatch(push("/login"));
       }
     }
 
