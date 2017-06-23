ba8ab0c60e79a973884b0937ffccf0cbb12cf4a6
houkah26 <houkah26@gmail.com>
2017-06-23 12:03:26 -0700

    add nav menu to header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Menu } from 'semantic-ui-react';
+import { logoutUser } from '../../actions';
 
-const Header = () => (
-  <div>
-    <h1>Header!</h1>
-  </div>
-)
+class Header extends Component {
+  state = { activeItem: 'home' }
 
-export default Header;
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+          <Menu.Menu position='right'>
+            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.props.logoutUser} />
+          </Menu.Menu>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { content: state.auth.content }
+}
+
+export default connect(mapStateToProps, { logoutUser })(Header);
