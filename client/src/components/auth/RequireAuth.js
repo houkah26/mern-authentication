89b1887888acb4a77682a3d5f2ddf5bd96686694
houkah26 <houkah26@gmail.com>
2017-06-23 12:00:03 -0700

    remove comments and logging

--- client/src/components/auth/RequireAuth.js
+++ client/src/components/auth/RequireAuth.js
@@ -3,20 +3,14 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {  
   class Authentication extends Component {
-    // static contextTypes = {
-    //   router: React.PropTypes.object
-    // }
-
     componentWillMount() {
       if(!this.props.authenticated) {
-        console.log("component will mount this.props.autheticated = " + this.props.authenticated);
         this.props.history.push("/login");
       }
     }
 
     componentWillUpdate(nextProps) {
       if(!nextProps.authenticated) {
-        console.log("component will update nextprops.autheticated = " + this.props.authenticated);
         this.props.history.push("/login");
       }
     }
