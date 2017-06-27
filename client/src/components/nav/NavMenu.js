0510a18d55e83bef61cc4630af33c9db573c0b62
houkah26 <houkah26@gmail.com>
2017-06-27 10:22:54 -0700

    conditional rendering of auth menu items based on auth status

--- client/src/components/nav/NavMenu.js
+++ client/src/components/nav/NavMenu.js
@@ -14,14 +14,15 @@ class NavMenu extends Component {
 
   render() {
     const activeItem = this.props.currentPath.slice(1);
+    const authenticated = this.props.authenticated;
 
     return (
       <Menu className='nav-menu' stackable inverted size='huge'>
         <Menu.Item name='homepage' active={activeItem === ''} onClick={this.navToRoute} />
         <Menu.Item name='dashboard' active={activeItem === 'dashboard'} onClick={this.navToRoute} />
-          <Menu.Item position='right' name='login' active={activeItem === 'login'} onClick={this.navToRoute} />
-          <Menu.Item name='register' active={activeItem === 'register'} onClick={this.navToRoute} />
-          <Menu.Item name='logout' onClick={this.props.logoutUser} />
+        {authenticated && <Menu.Item position='right' name='logout' onClick={this.props.logoutUser} />}
+        {!authenticated && <Menu.Item position='right' name='login' active={activeItem === 'login'} onClick={this.navToRoute} />}
+        {!authenticated && <Menu.Item name='register' active={activeItem === 'register'} onClick={this.navToRoute} />}
       </Menu>
     )
   }
