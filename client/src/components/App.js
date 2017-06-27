7e2d3a6f8d0856db419eff0dbbafa9f326d0780e
houkah26 <houkah26@gmail.com>
2017-06-27 11:26:38 -0700

    add hot module replacement

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'; 
+import { Provider } from 'react-redux';  
+import { ConnectedRouter } from 'react-router-redux';
+import cookie from 'react-cookie';
+
+import store, { history } from '../store'
+import { AUTH_USER } from '../actions/types';
 
 import Layout from './layout'
 
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import './App.css'; 
 
-const App = () => (
-    <Layout />
-)
+// Check for existing token and auth user if exists
+const token = cookie.load('token');
+if (token) {  
+  store.dispatch({ type: AUTH_USER });
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Layout />
+        </ConnectedRouter>
+      </Provider>
+    )
+  }
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';  
-import { ConnectedRouter } from 'react-router-redux';
-import cookie from 'react-cookie';
 
-import store, { history } from './store'
 import App from './components/App';
-import { AUTH_USER } from './actions/types';
 
-import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
-// Check for existing token and auth user if exists
-const token = cookie.load('token');
-if (token) {  
-  store.dispatch({ type: AUTH_USER });
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App/>, rootEl);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(<App />, rootEl)
+  })
 }
 
-const target = document.querySelector('#root')
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
-  target
-);
 registerServiceWorker();
