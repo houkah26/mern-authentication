bf5edd97bfc90d54c75e1761cee3df6a741c49df
houkah26 <houkah26@gmail.com>
2017-07-14 16:49:09 -0700

    change main content container

--- client/src/components/layout/MainContent.js
+++ client/src/components/layout/MainContent.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
 
 import Routes from '../pages/Routes';
 
 const MainContent = () => (
-  <Container className='main-contet'>
+  <div className='main-content'>
     <Routes />
-  </Container>
+  </div>
 )
 
 export default MainContent;
