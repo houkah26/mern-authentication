e7f22279a104313b914228f18194aa676802dd3f
houkah26 <houkah26@gmail.com>
2017-07-24 16:07:26 -0700

    move csv to array function to helper functions folder to be used by both user and stock controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,12 +1,14 @@
 const axios = require('axios'),
       User = require('../models/users'),
       helperFunctions = require('./helperFunctions');
+      setUserInfoForResponse = helperFunctions.setUserInfoForResponse;
+      CSVToArray = helperFunctions.CSVToArray;
 
 //========================================
 // User Info Route
 //========================================
 exports.getInfo = (req, res) => {
-  userInfo = helperFunctions.setUserInfoForResponse(req.user);
+  userInfo = setUserInfoForResponse(req.user);
 
   res.status(200).json({
     user: userInfo
@@ -34,7 +36,7 @@ exports.addFunds = (req, res, next) => {
         user.save((err, user) => {
           if (err) { return next(err); }
 
-          const userInfo = helperFunctions.setUserInfoForResponse(user);
+          const userInfo = setUserInfoForResponse(user);
           
           // respond with updated user
           res.status(200).json({
@@ -71,7 +73,7 @@ exports.getPortfolio = (req, res, next) => {
 
   axios.get(`http://download.finance.yahoo.com/d/quotes.csv?f=sl1&s=${symbolsString}`)
     .then(response => {
-      const dataArray = CSVToArray(response.data).slice(0, -1) ;
+      const dataArray = CSVToArray(response.data);
       
       const portfolioWithPrices = mapPrices(portfolio, dataArray);
 
@@ -121,92 +123,4 @@ const mapPrices = (portfolio, priceArray) => {
     })
     return stockWithPrice;
   })
-}
-
-// Convert CSV to array
-// ref: http://stackoverflow.com/a/1293163/2343
-// This will parse a delimited string into an array of
-// arrays. The default delimiter is the comma, but this
-// can be overriden in the second argument.
-const CSVToArray = (strData, strDelimiter) => {
-    // Check to see if the delimiter is defined. If not,
-    // then default to comma.
-    strDelimiter = (strDelimiter || ",");
-
-    // Create a regular expression to parse the CSV values.
-    var objPattern = new RegExp(
-        (
-            // Delimiters.
-            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
-            // Quoted fields.
-            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
-            // Standard fields.
-            "([^\"\\" + strDelimiter + "\\r\\n]*))"
-        ),
-        "gi"
-        );
-
-
-    // Create an array to hold our data. Give the array
-    // a default empty first row.
-    var arrData = [[]];
-
-    // Create an array to hold our individual pattern
-    // matching groups.
-    var arrMatches = null;
-
-
-    // Keep looping over the regular expression matches
-    // until we can no longer find a match.
-    while (arrMatches = objPattern.exec( strData )){
-
-        // Get the delimiter that was found.
-        var strMatchedDelimiter = arrMatches[ 1 ];
-
-        // Check to see if the given delimiter has a length
-        // (is not the start of string) and if it matches
-        // field delimiter. If id does not, then we know
-        // that this delimiter is a row delimiter.
-        if (
-            strMatchedDelimiter.length &&
-            strMatchedDelimiter !== strDelimiter
-            ){
-
-            // Since we have reached a new row of data,
-            // add an empty row to our data array.
-            arrData.push( [] );
-
-        }
-
-        var strMatchedValue;
-
-        // Now that we have our delimiter out of the way,
-        // let's check to see which kind of value we
-        // captured (quoted or unquoted).
-        if (arrMatches[ 2 ]){
-
-            // We found a quoted value. When we capture
-            // this value, unescape any double quotes.
-            strMatchedValue = arrMatches[ 2 ].replace(
-                new RegExp( "\"\"", "g" ),
-                "\""
-                );
-
-        } else {
-
-            // We found a non-quoted value.
-            strMatchedValue = arrMatches[ 3 ];
-
-        }
-
-
-        // Now that we have our value string, let's add
-        // it to the data array.
-        arrData[ arrData.length - 1 ].push( strMatchedValue );
-    }
-
-    // Return the parsed data.
-    return( arrData );
 }
