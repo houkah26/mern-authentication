d5daa41db525bacd3a9bda10aa9adcdc1cde8ce0
houkah26 <houkah26@gmail.com>
2017-07-26 10:43:32 -0700

    prettier formatting

--- client/src/components/pages/Dashboard/Portfolio.js
+++ client/src/components/pages/Dashboard/Portfolio.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import cookie from 'react-cookie';
-import { round } from 'lodash';
+import React, { Component } from "react";
+import axios from "axios";
+import cookie from "react-cookie";
+import { round } from "lodash";
 
-import { API_URL }from '../../../constants';
+import { API_URL } from "../../../constants";
 
-import Loading from '../../loading';
-import Table from '../../table';
+import Loading from "../../loading";
+import Table from "../../table";
 
 const tableHeaders = [
-  {name: 'Symbol', key: 'stockSymbol'},
-  {name: 'Name', key: 'stockName'},
-  {name: 'Shares', key: 'totalShares'},
-  {name: 'Price Per Share ($)', key: 'price'},
-  {name: 'Total Value ($)', key: 'total'}
-]
+  { name: "Symbol", key: "stockSymbol" },
+  { name: "Name", key: "stockName" },
+  { name: "Shares", key: "totalShares" },
+  { name: "Price Per Share ($)", key: "price" },
+  { name: "Total Value ($)", key: "total" }
+];
 
 export default class Portfolio extends Component {
   state = {
     portfolio: [],
     totalValue: 0
-  }
+  };
 
   componentDidMount() {
     this.fetchPortfolio();
   }
 
   fetchPortfolio = () => {
-    const token = cookie.load('token');
+    const token = cookie.load("token");
 
-    axios.get(`${API_URL}/user/portfolio`, {
-      headers: { 'Authorization': token }
-    })
+    axios
+      .get(`${API_URL}/user/portfolio`, {
+        headers: { Authorization: token }
+      })
       .then(response => {
         // calculate total value
         const totalValue = calcTotalValue(response.data.portfolio);
@@ -39,15 +40,15 @@ export default class Portfolio extends Component {
         // round prices to two decimals
         const portfolio = roundPrices(response.data.portfolio);
 
-        this.setState({ 
+        this.setState({
           portfolio: portfolio,
           totalValue: totalValue
-         })
+        });
       })
       .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   render() {
     const { portfolio, totalValue } = this.state;
@@ -55,33 +56,32 @@ export default class Portfolio extends Component {
 
     return (
       <div>
-        {portfolio.length === 0 ?
-          <Loading /> :
-          <Table
-            tableData={portfolio}
-            tableHeaders={tableHeaders}
-            tableFooter={tableFooter}
-          />
-        }
+        {portfolio.length === 0
+          ? <Loading />
+          : <Table
+              tableData={portfolio}
+              tableHeaders={tableHeaders}
+              tableFooter={tableFooter}
+            />}
       </div>
-    )
+    );
   }
 }
 
 // Round prices to two decimals
-const roundPrices = (stockData) => {
+const roundPrices = stockData => {
   return stockData.map(stock => {
     stock.price = round(stock.price, 2).toFixed(2);
     stock.total = round(stock.total, 2).toFixed(2);
     return stock;
-  })
-}
+  });
+};
 
 // Calculate Total Value of Entire Portfolio
-const calcTotalValue = (portfolio) => {
+const calcTotalValue = portfolio => {
   let total = 0;
   portfolio.forEach(stock => {
     total += stock.total;
   });
   return round(total, 2).toFixed(2);
-}
+};
