3b6eecfd5410fe50630e2583e0e3fdd5de52ac57
houkah26 <houkah26@gmail.com>
2017-07-26 16:04:06 -0700

    prettier formatting

--- client/src/components/form/renderFields.js
+++ client/src/components/form/renderFields.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import RenderField from './RenderField';
-import { Field } from 'redux-form';  
+import React from "react";
+import RenderField from "./RenderField";
+import { Field } from "redux-form";
 
-const renderFields = (inputFields) => (
-  inputFields.map(field => (
-    <Field 
+const renderFields = inputFields =>
+  inputFields.map(field =>
+    <Field
       name={field.name}
       type={field.type}
       component={RenderField}
       key={field.name}
     />
-  ))
-)
+  );
 
 export default renderFields;
-
