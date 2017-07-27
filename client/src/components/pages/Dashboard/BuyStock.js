dac46aa6d689cf1c65d48cd3ec0140f4bb2fc014
houkah26 <houkah26@gmail.com>
2017-07-27 10:12:06 -0700

    add divider to grid

--- client/src/components/pages/Dashboard/BuyStock.js
+++ client/src/components/pages/Dashboard/BuyStock.js
@@ -5,7 +5,7 @@ import QuoteStockForm from "../../form/QuoteStockForm";
 import BuyStockForm from "../../form/BuyStockForm";
 
 const BuyStock = () =>
-  <Grid stackable columns={2}>
+  <Grid stackable columns={2} divided>
     <Grid.Column>
       <QuoteStockForm />
     </Grid.Column>
