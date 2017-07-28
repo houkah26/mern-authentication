cf26d1bfbeb7d672fad02447cf851a7d6415a8e6
houkah26 <houkah26@gmail.com>
2017-07-28 16:40:03 -0700

    add confirmation password, prettier formatting

--- client/src/components/form/RegisterForm.js
+++ client/src/components/form/RegisterForm.js
@@ -1,84 +1,98 @@
-import React, { Component } from 'react';  
-import { connect } from 'react-redux';  
-import { Field, reduxForm } from 'redux-form';  
-import { Form, Message, Icon } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { Form, Message, Icon } from "semantic-ui-react";
 
-import { registerUser, clearAuthErrors } from '../../actions/auth';
+import { registerUser, clearAuthErrors } from "../../actions/auth";
 
-import renderFields from './renderFields';
-import Checkbox from './Checkbox';
+import renderFields from "./renderFields";
+import Checkbox from "./Checkbox";
 
 // Input fields to render
 const inputFields = [
-  {name: 'firstName', type: 'text'},
-  {name: 'lastName', type: 'text'},
-  {name: 'username', type: 'text'},
-  {name: 'password', type: 'password'},
-]
+  { name: "firstName", type: "text" },
+  { name: "lastName", type: "text" },
+  { name: "username", type: "text" },
+  { name: "password", type: "password" },
+  { name: "confirmPassword", type: "password" }
+];
 
 // Form validationg for redux-form
 const validate = formProps => {
   const errors = {};
 
   if (!formProps.firstName) {
-    errors.firstName = 'Please enter a first name';
+    errors.firstName = "Please enter a first name";
   }
 
   if (!formProps.lastName) {
-    errors.lastName = 'Please enter a last name';
+    errors.lastName = "Please enter a last name";
   }
 
   if (!formProps.username) {
-    errors.username = 'Please enter an username';
+    errors.username = "Please enter an username";
   }
 
   if (!formProps.password) {
-    errors.password = 'Please enter a password';
+    errors.password = "Please enter a password";
+  }
+
+  if (formProps.password !== formProps.confirmPassword) {
+    errors.confirmPassword = "Passwords do not match";
   }
 
   if (!formProps.checkbox) {
-    errors.checkbox = 'You must accept the Terms and Conditions';
+    errors.checkbox = "You must accept the Terms and Conditions";
   }
 
   return errors;
-}
+};
 
 class RegisterForm extends Component {
   componentDidMount() {
     this.props.clearAuthErrors();
   }
 
-  handleFormSubmit = (formProps) => {
+  handleFormSubmit = formProps => {
     this.props.registerUser(formProps);
-  }
+  };
 
   render() {
     const { handleSubmit, errorMessage } = this.props;
     const containsError = errorMessage.length > 0;
 
     return (
-      <Form error={containsError} onSubmit={handleSubmit(this.handleFormSubmit)}>
-        <Message error header='Error:' content={errorMessage}/>
+      <Form
+        error={containsError}
+        onSubmit={handleSubmit(this.handleFormSubmit)}
+      >
+        <Message error header="Error:" content={errorMessage} />
         <Form.Group widths="equal">
           {renderFields(inputFields.slice(0, 2))}
         </Form.Group>
         {renderFields(inputFields.slice(2))}
-        <Field name="checkbox" component={Checkbox} label="I agree to the Terms and Conditions" />
+        <Field
+          name="checkbox"
+          component={Checkbox}
+          label="I agree to the Terms and Conditions"
+        />
         <Form.Button>
-          <Icon name='signup' />Register
+          <Icon name="signup" />Register
         </Form.Button>
       </Form>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {errorMessage: state.auth.error}
-}
+const mapStateToProps = state => {
+  return { errorMessage: state.auth.error };
+};
 
-const createForm = reduxForm({  
-  form: 'register',
+const createForm = reduxForm({
+  form: "register",
   validate
 });
 
-export default connect(mapStateToProps, { registerUser, clearAuthErrors })(createForm(RegisterForm));
+export default connect(mapStateToProps, { registerUser, clearAuthErrors })(
+  createForm(RegisterForm)
+);
