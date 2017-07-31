c75fd463e18e98bd3d0c8802d40c3836d578890e
houkah26 <houkah26@gmail.com>
2017-07-31 10:50:29 -0700

    fix sticky footer in layout styles

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
-import './index.css';
+import "./index.css";
 
-const Layout = () => (
-  <div className="layout">
+const Layout = () =>
+  <div className="layout-container">
     <Header />
-    <MainContent/>
-    <Footer/>
-  </div>
-)
+    <MainContent />
+    <Footer />
+  </div>;
 
 export default Layout;
